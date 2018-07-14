9886158ab8bd853b3307c0768d546a4a2411be8f
Carrie Savinell <carrielane89@gmail.com>
2018-07-14 14:13:48 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,37 +1,20 @@
 class PigLatinizer
 
-  +    attr_accessor :word
-  +
-  +    def initialize
-  +    end
-  +
-  +    def piglatinize(sentence)
-  +
-  +        words = sentence.split(" ")
-  +        output = []
-  +
-  +        words.each do |word|
-  +            count = 0
-  +            word.split("").each do |i|
-  +              if i =~ /[^AEIOUaeiou]/
-  +                  count += 1
-  +              elsif i =~ /[AEIOUaeiou]/
-  +                  if word[0] =~ /[AEIOUaeiou]/
-  +                    output << "#{word}way"
-  +                  elsif word[0..count] =~ /[^AEIOUaeiou]{#{count}}/
-  +                    output << "#{word[count..-1]}#{word[0..count-1]}ay"
-  +                  else
-  +                    output << "#{word[1..-1]}#{word[0]}ay"
-  +                  end
-  +                  count = 0
-  +
-  +                  break
-  +              end
-  +            end
-  +        end
-  +
-  +      return output.join(" ")
-  +
-  +    end
-  +
-  +end
+  +    +  attr_accessor :text
++  def piglatinize(text)
++    @text = text.downcase
++    vowel ='aeiou'
++    text = text.split()
++      text.collect do |word|
++        if word.length == 1
++            "#{word}way"
++        elsif vowel.include? word[0].downcase
++            "#{word}way"
++        else
++            split = word.split(/([aeiou])/,2)
++            "#{split[1]}#{split[2]}#{split[0]}ay"
++        end
++    end.join(' ')
++   end
++
++end
