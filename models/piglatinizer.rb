e83d7736e48ca212aedc3616e705f855eab48e72
Carrie Savinell <carrielane89@gmail.com>
2018-07-14 14:08:07 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,18 +7,27 @@ class PigLatinizer
   end
 
 
-  def piglatinize(text)
-    #split words in an array
-    words = text.split(" ")
-
-    #iterate over words and add
-    words_array = words.collect do |word|
-      vowel_i = words.index('#{word.scan(/[AEIOUaeiou]).first}')
-      sliced_words = word.slice!(0,vowel_i)
-
-      puts '#{vowel_i}'
-      word << "#{sliced_words} #{"w" if vowel_i == 0}ay"
-      end
-      new_phrase.join(" ")
-   end
-end
+  +  def initialize
+  +    # @text = text
+  +  end
+  +
+  +  def piglatinize(text)
+  +    @text = text
+  +    #split text into an array of words
+  +    words = @text.split(" ")
+  +    #collect pig latin versions of words
+  +    pig_words = words.map do |word|
+  +      #find first vowel as an index of the array ([word] per iteration)
+  +      vowel_index = word.index("#{word.scan(/[AEIOUaeiou]/).first}") #first scanned vowel's index
+  +      #slice! deletes and returns deleted content
+  +      removed_portion = word.slice!(0,vowel_index) #remove form start until vowel (can => c)
+  +      #take remaining portion and add removed_portion to end + ay(way depending on if first letter == vowel)
+  +      #ex1:egg=> eggway --ex2:dck => uck + d + ay => uckday
+  +      puts "#{vowel_index}"
+  +      word << "#{removed_portion}#{"w" if vowel_index == 0}ay" #0 indicates first letter == vowel
+  +    end
+  +    #put pig words together as spaced text
+  +    pig_words.join(" ")
+  +  end
+  +
+  +end
