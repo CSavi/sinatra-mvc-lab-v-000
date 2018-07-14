10f3bf11faccc49c2aab012e5bbd5c71fd36c013
Carrie Savinell <carrielane89@gmail.com>
2018-07-14 14:05:30 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,8 +3,8 @@ class PigLatinizer
   attr_reader :text
 
   def initialize
-    @text = text 
-  end   
+    @text = text
+  end
 
 
   def piglatinize(text)
@@ -20,25 +20,5 @@ class PigLatinizer
       word << "#{sliced_words} #{"w" if vowel_i == 0}ay"
       end
       new_phrase.join(" ")
-   end 
-end       
-      
-      if word.slice(0,1) =~ (/[AEIOUaeiou]/)
-        word = word + "way"
-      else
-        word = word.slice(1,word.size) + word.slice(0,1)
-        if  word.slice(0,1) =~ (/[aeiou]/)
-          word = word + "ay"
-        else
-          word = word.slice(1,word.size) + word.slice(0,1)
-          if word.slice(0,1) =~ (/[aeiou]/)
-            word = word + "ay"
-          else
-            word = word.slice(1, word.size) + word.size(0,1) + "ay"
-          end
-        end
-      end
-    end
-  new_phrase = word.join(" ")
- end
+   end
 end
